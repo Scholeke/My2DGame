da9e33d635ab89d7c141c01608f3136d5c7bf1c8
RobeirDenExpeir <91462750+RobeirDenExpeir@users.noreply.github.com>
2023-08-17 01:06:34 +0200

    Update RectangleExtension.cs

--- My2DGame/Core/RectangleExtension.cs
+++ My2DGame/Core/RectangleExtension.cs
@@ -12,7 +12,7 @@ namespace My2DGame.Core
     {
         public static bool TouchTopOf(this Rectangle r1, Rectangle r2)
         {
-            return r1.Bottom >= r2.Top - 1 &&
+            return r1.Bottom >= r2.Top + 1 &&
                     r1.Bottom <= r2.Top + r2.Height / 2 &&
                     r1.Right >= r2.Left + r2.Width / 5 &&
                     r1.Left <= r2.Right - r2.Width / 5;
@@ -20,8 +20,8 @@ namespace My2DGame.Core
 
         public static bool TouchBottomOf(this Rectangle r1, Rectangle r2)
         {
-            return r1.Top <= r2.Bottom + r2.Height / 5 &&
-                    r1.Top >= r2.Bottom - 1 &&
+            return r1.Top + 10 <= r2.Bottom + r2.Height / 5 &&
+                    r1.Top + 10 >= r2.Bottom - 1 &&
                     r1.Right >= r2.Left + r2.Width / 5 &&
                     r1.Left <= r2.Right - r2.Width / 5;
         }
@@ -30,16 +30,14 @@ namespace My2DGame.Core
             return r1.Right <= r2.Right &&
                    r1.Right >= r2.Left - 5 &&
                    r1.Top <= r2.Bottom - r2.Width / 4 &&
-                   r1.Bottom >= r2.Top + r2.Width / 4
-                ;
+                   r1.Bottom >= r2.Top + r2.Width / 4;
         }
         public static bool TouchRightOf(this Rectangle r1, Rectangle r2)
         {
             return r1.Left >= r2.Left &&
                    r1.Left <= r2.Right - 5 &&
                    r1.Top <= r2.Bottom - r2.Width / 4 &&
-                   r1.Bottom >= r2.Top + r2.Width / 4
-                ;
+                   r1.Bottom >= r2.Top + r2.Width / 4;
         }
     }
 }
