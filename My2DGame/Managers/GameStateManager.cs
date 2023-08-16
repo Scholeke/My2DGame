b46ed1d538efcd11b1cea61e9aa93ab35b69b8a1
RobeirDenExpeir <91462750+RobeirDenExpeir@users.noreply.github.com>
2023-08-17 01:09:05 +0200

    Refactor GameStateManager

--- My2DGame/Managers/GameStateManager.cs
+++ My2DGame/Managers/GameStateManager.cs
@@ -15,85 +15,34 @@ namespace My2DGame.Managers
 {
     internal class GameStateManager : IComponent
     {
-        private List<Level> levels = new List<Level>();
-        private MenuScene _ms = new MenuScene();
-        private GameScene _gs;
-        private GameOverScene _gOs = new GameOverScene();
-        private GameWonScene _gws = new GameWonScene();
-        private EndScene _es = new EndScene();
-        private ContentManager _content;
-        private int currentLevel;
+        private Dictionary<Data.States, State> _stateMap;
+        private State _currentScene;
         public void LoadContent(ContentManager content)
         {
-            levels.Add(new Level1(content));
-            levels.Add(new Level2(content));
-            _content = content;
-            _gs = new GameScene(levels[currentLevel]);
-            _ms.LoadContent(content);
-            _gs.LoadContent(content);
-            _gOs.LoadContent(content);
-            _gws.LoadContent(content);
-            _es.LoadContent(content);
+            _stateMap = new Dictionary<Data.States, State>
+            {
+                { Data.States.Menu, new MenuState() },
+                { Data.States.Game, new GameState() },
+                { Data.States.GameOver, new GameOverState() },
+                { Data.States.GameWon, new GameWonState() },
+                { Data.States.End, new EndState() }
+            };
+            _currentScene = _stateMap[Data.CurrentState];
+            foreach (State s in _stateMap.Values)
+            {
+                s.LoadContent(content);
+            }
         }
 
         public void Update(GameTime gameTime)
         {
-            switch(Data.CurrentState)
-            {
-                case Data.Scenes.Menu:
-                    _ms.Update(gameTime);
-                    break;
-                case Data.Scenes.Game:
-                    _gs.Update(gameTime);
-                    break;
-                case Data.Scenes.GameOver:
-                    _gOs.Update(gameTime);
-                    if (_gOs.NextGame)
-                        _gs = _gOs.NewGame(_content);
-                    break;
-                case Data.Scenes.GameWon:
-                    _gws.Update(gameTime);
-                    if (_gws.NextLevel)
-                    {
-                        currentLevel++;
-                        if(currentLevel < levels.Count)
-                            _gs = _gws.NewLevel(_content, levels[currentLevel]);
-                    }
-                    if(currentLevel >= levels.Count)
-                    {
-                        Data.CurrentState = Data.Scenes.End;
-                    }
-                    break;
-                case Data.Scenes.End:
-                    _es.Update(gameTime);
-                    break;
-                default:
-                    break;
-            }
+            _currentScene = _stateMap[Data.CurrentState];
+            _currentScene.Update(gameTime);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            switch (Data.CurrentState)
-            {
-                case Data.Scenes.Menu:
-                    _ms.Draw(spriteBatch);
-                    break;
-                case Data.Scenes.Game:
-                    _gs.Draw(spriteBatch);
-                    break;
-                case Data.Scenes.GameOver:
-                    _gOs.Draw(spriteBatch);
-                    break;
-                case Data.Scenes.GameWon:
-                    _gws.Draw(spriteBatch);
-                    break;
-                case Data.Scenes.End:
-                    _es.Draw(spriteBatch);
-                    break;
-                default:
-                    break;
-            }
+            _currentScene.Draw(spriteBatch);
         }
     }
 }
